a48fe01a75ba146c4b1be2d04232551016098f33
Matteo Valenzi <dilloawallace@hotmail.com>
2025-04-24 11:17:48 +0200

    enums

---
+++ MyEnums.cs
@@ -0,0 +1,33 @@
+// enum
+// tipo che può assumere solo specifici valori
+
+enum Stagione
+{
+    Primavera,
+    Estate,
+    Autunno,
+    Inverno
+}
+
+enum Mese
+{
+    Gennaio = 1,
+    Febbraio = 2,
+    Marzo = 3,
+    Aprile = 4,
+    Maggio = 5,
+    Giugno = 6,
+    Luglio = 7,
+    Agosto = 8,
+    Settembre = 9,
+    Ottobre = 10,
+    Novembre = 11,
+    Dicembre = 12
+}
+
+enum HttpStatusCode : ushort
+{
+    OK = 200,
+    NotFound = 404,
+    ServerError = 500
+}
--- Program.cs
+++ Program.cs
@@ -40,3 +40,68 @@ else
 {
     Console.WriteLine("Sei minorenne, NON puoi essere assunto.");
 }
+
+string etichetta = "";
+
+etichetta = eta >= 18 ? "maggiorenne" : "minorenne";
+
+var stagioneCorrente = Stagione.Primavera;
+Stagione stagionePrecendente = Stagione.Inverno;
+
+Console.WriteLine(stagioneCorrente);
+
+// conversione
+int stagioneCorrenteInt = (int)stagioneCorrente;
+Console.WriteLine(stagioneCorrenteInt);
+
+var autunno = (Stagione)2;
+Console.WriteLine(autunno);
+
+if (stagioneCorrente == Stagione.Primavera)
+{
+    Console.WriteLine("In primavera sbocciano i fiori.");
+}
+
+Mese meseCorrente = Mese.Febbraio;
+Console.WriteLine((int)meseCorrente);
+
+switch (meseCorrente)
+{
+    case Mese.Gennaio:
+        Console.WriteLine($"{meseCorrente} è il mese numero {(int)meseCorrente}");
+        break;
+    case Mese.Febbraio:
+        Console.WriteLine($"{meseCorrente} è il mese numero {(int)meseCorrente}");
+        break;
+    case Mese.Marzo:
+        Console.WriteLine($"{meseCorrente} è il mese numero {(int)meseCorrente}");
+        break;
+    default:
+        Console.WriteLine("Altri mesi dopo Marzo");
+        break;
+}
+
+Console.Write("Inserire un mese (1-12): ");
+var m = Console.ReadLine();
+
+if (m != null)
+{
+    int meseInNumero = Convert.ToInt32(m);
+    meseCorrente = (Mese)meseInNumero;
+}
+
+switch (meseCorrente)
+{
+    case Mese.Novembre:
+    case Mese.Aprile:
+    case Mese.Giugno:
+    case Mese.Settembre:
+        Console.WriteLine($"{meseCorrente} ha 30 giorni.");
+        break;
+    case Mese.Febbraio:
+        Console.WriteLine($"{meseCorrente} ha 28 giorni (a volte 29).");
+        break;
+    default:
+        Console.WriteLine($"Tutti gli altri ne hanno 31, compreso {meseCorrente}");
+        break;
+}
